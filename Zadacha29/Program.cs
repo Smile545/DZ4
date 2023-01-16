29ae59d89c676e2241cab82f6f2c7675fa7e42bb
Andrey <an.nikishin2012@yandex.ru>
2023-01-16 20:51:51 +0400

    Решил 29 задачу

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задайте длину массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+{
+    array[i] =new  Random().Next(0, 100);
+    Console.Write(array[i] +  ", ");
+}
